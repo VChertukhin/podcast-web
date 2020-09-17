50dfc29a300e258c822b06668c6a28ac244faa04
Suvorov Daniil <severecloud@gmail.com>
2020-09-18 00:42:23 +0300

    fix icons class name

--- src/components/icons/Icon24Cut.ts
+++ src/components/icons/Icon24Cut.ts
@@ -25,13 +25,13 @@ function mountIcon() {
     isMounted = true;
   }
 }
-export interface Icon24AddCircleProps extends HTMLAttributes<HTMLDivElement> {
+export interface Icon24CutProps extends HTMLAttributes<HTMLDivElement> {
   fill?: string;
   width?: number;
   height?: number;
   getRootRef?: RefCallback<HTMLDivElement> | RefObject<HTMLDivElement>;
 }
-const Icon24AddCircle: FC<Icon24AddCircleProps> = (props) => {
+const Icon24Cut: FC<Icon24CutProps> = (props) => {
   useIsomorphicLayoutEffect(() => {
     mountIcon();
   }, []);
@@ -45,5 +45,5 @@ const Icon24AddCircle: FC<Icon24AddCircleProps> = (props) => {
     }),
   );
 };
-(Icon24AddCircle as any).mountIcon = mountIcon;
-export default Icon24AddCircle;
+(Icon24Cut as any).mountIcon = mountIcon;
+export default Icon24Cut;
